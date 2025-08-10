54971eb319791a476b8d3ab3cb717184d39b09c6
david.clegg <david.clegg@gov.scot>
2025-08-10 14:57:59 +0100

    general refactor od exisitng functions

--- Log_Detective/Log_Detective.py
+++ Log_Detective/Log_Detective.py
@@ -5,25 +5,30 @@
 #Generate Report
 
 
-##Takes a file extension and return an array of lines
+##Takes a file extension and return a array of strings
 def read_file(file_ext):
     file = open(file_ext)
     lines = []
     for line in file:
-        lines.append(line)
-    return lines  
+        lines.append(line.replace("\n", ""))
+    file.close()
+    return lines
 
     
-##Function takes an array of strings and a keyword, and filters the strings that contain the keyword
-def line_keyword_check(text_lines, keyword):
+# Function takses 
+def text_error_check(text_lines):
     if text_lines is None:
         raise Exception("Missing arguments -> text_lines")
-    if keyword is None:
-        raise Exception("Missing arguments -> keyword")
-    checked_lines = []
+    error_lines = []
+    critical_lines = []
     for line in text_lines:
-        if keyword in line:
-            checked_lines.append(line)
-    return checked_lines
+        if "ERROR" in line:
+            error_lines.append(line)
+        if "CRITICAL" in line:
+            critical_lines.append(line)
+    return error_lines, critical_lines
 
-print(line_keyword_check(read_file("test.txt"), "ERROR"))
+
+    
+
+print(read_file("test.txt"))
