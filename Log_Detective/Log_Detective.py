848a8f35abec619a9cbc9ec171c7ed17720673a1
david.clegg <david.clegg@gov.scot>
2025-08-10 14:40:27 +0100

    add function to filter text lines

--- Log_Detective/Log_Detective.py
+++ Log_Detective/Log_Detective.py
@@ -5,12 +5,25 @@
 #Generate Report
 
 
-##Takes a file extension and returns the contents as a concatenated string
+##Takes a file extension and return an array of lines
 def read_file(file_ext):
     file = open(file_ext)
-    lines = ""
+    lines = []
     for line in file:
-        lines += line
-    return lines    
+        lines.append(line)
+    return lines  
 
-print(read_file("test.txt"))
+    
+##Function takes an array of strings and a keyword, and filters the strings that contain the keyword
+def line_keyword_check(text_lines, keyword):
+    if text_lines is None:
+        raise Exception("Missing arguments -> text_lines")
+    if keyword is None:
+        raise Exception("Missing arguments -> keyword")
+    checked_lines = []
+    for line in text_lines:
+        if keyword in line:
+            checked_lines.append(line)
+    return checked_lines
+
+print(line_keyword_check(read_file("test.txt"), "ERROR"))
