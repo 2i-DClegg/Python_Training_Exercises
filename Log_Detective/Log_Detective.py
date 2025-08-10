7500e6fe95647f4103f47e0e6ddfea5bbbaa4af2
david.clegg <david.clegg@gov.scot>
2025-08-10 14:29:53 +0100

    wrote read file function

--- Log_Detective/Log_Detective.py
+++ Log_Detective/Log_Detective.py
@@ -4,3 +4,13 @@
 
 #Generate Report
 
+
+##Takes a file extension and returns the contents as a concatenated string
+def read_file(file_ext):
+    file = open(file_ext)
+    lines = ""
+    for line in file:
+        lines += line
+    return lines    
+
+print(read_file("test.txt"))
